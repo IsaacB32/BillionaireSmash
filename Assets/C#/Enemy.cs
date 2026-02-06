efbd0661d053ccf1f2691233364b337f45999012
Blaise Rettig <blaiserettig@hotmail.com>
2026-02-06 13:34:35 -0800

    movement refactor

--- Assets/C#/Enemy.cs
+++ Assets/C#/Enemy.cs
@@ -16,11 +16,13 @@ public class Enemy : MonoBehaviour
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
     private Transform _playerTransform;
+    private Rigidbody2D _rb;
 
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
     }
     
     void OnEnable()
@@ -41,20 +43,40 @@ public class Enemy : MonoBehaviour
         transform.localScale = Vector3.one * _currentStats.scale;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         _currentTtl -= Time.deltaTime;
         if (_currentTtl <= 0) Die();
 
-        transform.position = Vector3.MoveTowards(
+        /*transform.position = Vector3.MoveTowards(
             transform.position, 
             _playerTransform.position, 
             _currentStats.speed * Time.deltaTime
-        );
+        );*/
+        
+        Vector2 separation = ComputeSeparation();
+        Vector2 moveDir = ((Vector2)_playerTransform.position - _rb.position).normalized;
+        Vector2 finalVelocity = (moveDir + separation).normalized * _currentStats.speed;
+        _rb.MovePosition(_rb.position + finalVelocity * Time.fixedDeltaTime);
         
         if (Vector3.Distance(transform.position, Game.Instance.player.transform.position) < distanceThreshold) Die();
 
         _spriteRenderer.flipX = _playerTransform.position.x < transform.position.x;
+    }
+    
+    Vector2 ComputeSeparation()
+    {
+        Vector2 separationVec = Vector2.zero;
+        Collider2D[] neighbors = Physics2D.OverlapCircleAll(_rb.position, 0.1f); 
+
+        foreach (var neighbor in neighbors)
+        {
+            if (neighbor.gameObject == gameObject) continue;
+
+            Vector2 diff = _rb.position - (Vector2)neighbor.transform.position;
+            separationVec += diff.normalized / diff.magnitude;
+        }
+        return separationVec;
     }
 
     void Die()
