83dc64709e7d03335f476f534a774550452e0b9e
Blaise Rettig <blaiserettig@hotmail.com>
2026-02-07 22:12:39 -0800

    health done

--- Assets/C#/Game.cs
+++ Assets/C#/Game.cs
@@ -40,6 +40,7 @@ public class Game : MonoBehaviour
     [Header("Game Score Text")]
     [SerializeField] private TextMeshProUGUI highscoreText;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI healthText;
     private int score = 0;
 
     [Header("Misc")]
@@ -78,6 +79,8 @@ public class Game : MonoBehaviour
             float playerHealthPercent = Instance.player.currentHealth / Instance.player.maxHealth;
             float target = Mathf.Lerp(0f, 3f, 1f - playerHealthPercent);
             damageMaterial.SetFloat("_VignetteIntensity", target);
+
+            healthText.text = $"{Instance.player.currentHealth}";
         }
     }
     
